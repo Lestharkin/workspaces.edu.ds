0a7c592f7f5fcab168180654561fc6acb528e0e8
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2023-08-01 14:51:53 -0500

    sync

---
+++ linkedlist/src/main/java/edu/nodes/Node.java
@@ -0,0 +1,58 @@
+package edu.nodes;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import edu.interfaces.node.NodeInterface;
+
+public class Node<T> implements NodeInterface<T> {
+
+  private T object;
+
+  @Override
+  public boolean setObject(T object) {
+    try {
+      this.object = object; 
+      return true;     
+    } catch (Exception e) {
+      Logger.getLogger(this.getClass().getName()).log(Level.WARNING, e.getMessage(), e);
+      return false;
+    }
+  }
+
+  @Override
+  public T getObject() {
+    return this.object;
+  }
+
+  @Override
+  public boolean isEquals(T object) {
+    try {
+      return this.object.toString().equals(object.toString());
+    } catch (Exception e) {
+      Logger.getLogger(this.getClass().getName()).log(Level.WARNING, e.getMessage(), e);
+      return false;
+    }
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public Node<T> getClone() {
+    try {
+      if (object != null) {
+        return (Node<T>) this.clone();
+      }
+    } catch (CloneNotSupportedException e) {
+      Logger.getLogger(this.getClass().getName()).log(Level.WARNING, e.getMessage(), e);
+    }
+    return null;
+  }
+
+  @Override
+  public String toString() {
+    return "Node [object=" + object + "]";
+  }
+  
+  
+
+}
---
+++ linkedlist/src/test/java/edu/mockobject/MockObject.java
@@ -0,0 +1,45 @@
+package edu.mockobject;
+
+public class MockObject {
+
+  private int integer;
+  private String string;
+  private char[] charArray;
+
+  public MockObject() {
+    integer = 0;
+    string = "";
+    charArray = new char[0];
+  }
+
+  public MockObject(int integer, String string, char[] charArray) {
+    this.string = string;
+    this.integer = integer;
+    this.charArray = charArray;
+  }
+
+  public int getInteger() {
+    return integer;
+  }
+
+  public void setInteger(int integer) {
+    this.integer = integer;
+  }
+
+  public String getString() {
+    return string;
+  }
+
+  public void setString(String string) {
+    this.string = string;
+  }
+
+  public char[] getCharArray() {
+    return charArray;
+  }
+
+  public void setCharArray(char[] charArray) {
+    this.charArray = charArray;
+  }
+}
+
