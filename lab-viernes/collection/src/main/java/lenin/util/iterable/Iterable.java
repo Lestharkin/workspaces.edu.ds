dbdca2d0f5f1fdf330630e10d7ebd4e909536efb
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2024-01-26 15:29:44 -0500

    sync

---
+++ lab-viernes/collection/src/main/java/lenin/util/iterable/Iterable.java
@@ -0,0 +1,10 @@
+package lenin.util.iterable;
+
+import java.util.function.Function;
+
+import lenin.util.iterator.Iterator;
+
+public interface Iterable<E> {
+  public void forEach(Function<E, Void> action);
+  public Iterator<E> iterator(); 
+}
