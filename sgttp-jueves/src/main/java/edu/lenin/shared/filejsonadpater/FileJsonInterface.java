b7cde650e4a57b4cb8d762e9983dda129772cf5f
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2024-03-07 16:23:00 -0500

    sync

---
+++ sgttp-jueves/src/main/java/edu/lenin/shared/filejsonadpater/FileJsonInterface.java
@@ -0,0 +1,11 @@
+package edu.lenin.shared.filejsonadpater;
+
+public interface FileJsonInterface<E> {
+  E getObject(String pathFile, Class<E> classOfT);
+
+  E[] getObjects(String pathFile, Class<E[]> classOfT);
+
+  Boolean writeObject(String pathFile, E object);
+
+  Boolean writeObjects(String pathFile, E[] objects);
+}
