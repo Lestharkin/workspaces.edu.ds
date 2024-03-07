92024d8526810b1f4acacb2d23342222ad12b93a
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2024-03-07 17:03:44 -0500

    sync

--- sgttp-jueves/src/main/java/edu/lenin/model/repository/UserRepository.java
+++ sgttp-jueves/src/main/java/edu/lenin/model/repository/UserRepository.java
@@ -1,5 +1,27 @@
 package edu.lenin.model.repository;
 
+import edu.lenin.model.domain.User;
+import edu.lenin.shared.filejsonadpater.FileJsonAdapter;
+import edu.lenin.shared.filejsonadpater.FileJsonInterface;
+
 public class UserRepository {
   
+  private FileJsonInterface<UserEntity> fileJson;
+  private String pathFile;
+
+  public UserRepository(String pathFile) {
+    this.pathFile = pathFile;
+    this.fileJson = FileJsonAdapter.getInstance();
+  }
+
+  public User getUser(String username) {
+    UserEntity[] userEntities = fileJson.getObjects(pathFile, UserEntity[].class);
+    for (UserEntity userEntity : userEntities) {
+      if (userEntity.username.equals(username)) {
+        return new User();
+      }
+    }
+  }
+
+
 }
