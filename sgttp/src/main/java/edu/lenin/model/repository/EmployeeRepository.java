bcd215149ad98f13e3e15edc83af69bab5d5e3b6
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2024-03-08 15:23:06 -0500

    sync

---
+++ sgttp/src/main/java/edu/lenin/model/repository/EmployeeRepository.java
@@ -0,0 +1,31 @@
+package edu.lenin.model.repository;
+
+import com.lestharkin.io.array.Array;
+
+import edu.lenin.model.domain.Employee;
+import edu.lenin.shared.filejsonadapter.FileJsonAdapter;
+import edu.lenin.shared.filejsonadapter.FileJsonInterface;
+
+public class EmployeeRepository {
+  private FileJsonInterface<EmployeeEntity> fileJson;
+  private String pathFile;
+
+  public EmployeeRepository(String pathFile) {
+    this.pathFile = pathFile;
+    this.fileJson = FileJsonAdapter.getInstance();
+  }
+
+  public Employee getEmployee(String id) {
+    EmployeeEntity[] employeeEntities = fileJson.getObjects(pathFile, EmployeeEntity[].class);
+    for (EmployeeEntity employeeEntity : employeeEntities) {
+      if (employeeEntity.id.equals(id)) {
+        return new Employee(
+            employeeEntity.names,
+            employeeEntity.lastNames,
+            new Array<>(employeeEntity.phoneNumbers.split(",")),
+            employeeEntity.id);
+      }
+    }
+    return Employee.getNullEmployee();
+  }
+}
